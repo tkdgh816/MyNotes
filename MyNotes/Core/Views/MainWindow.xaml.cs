4fc2878aded37b71f96f268d193da8a75f31968e
박상호 <tkdgh816@outlook.com>
2025-03-30 22:49:46 +0900

    기본 네임스페이스와 디렉터리 구조 추가

---
+++ MyNotes/Core/Views/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyNotes.Views;
+
+public sealed partial class MainWindow : Window
+{
+  public MainWindow()
+  {
+    this.InitializeComponent();
+  }
+}
---
+++ MyNotes/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Runtime.InteropServices.WindowsRuntime;
+
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Controls.Primitives;
+global using Microsoft.UI.Xaml.Data;
+global using Microsoft.UI.Xaml.Input;
+global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Navigation;
+
+global using Windows.Foundation;
+global using Windows.Foundation.Collections;
